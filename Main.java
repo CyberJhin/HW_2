9ba99dedba62f95740b1ab5396acbe46e8c4b77c
CyberJhin <drkapp@mail.ru>
2024-01-09 21:12:40 +0300

    slow_work commit

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
+
+    private static String foo() {
+        return "Hello World!";
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(foo());
     }
 }
